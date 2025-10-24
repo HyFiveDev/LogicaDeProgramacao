b13669380a9cfafc54931c332fc71952f85c5585
HyFiveDev <kauan.souza@edu.fecap.br>
2025-10-24 18:42:47 -0300

    comit

---
+++ Kauan Alves de Souza e Giovanni da Silva Costa/Assets/Script/Timer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    void Update()
+    {
+
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else
+        {
+            remainingTime = 0;
+            timerText.color = Color.red;
+
+        }
+        if(remainingTime==0)
+        {
+            SceneManager.LoadScene("End");
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
